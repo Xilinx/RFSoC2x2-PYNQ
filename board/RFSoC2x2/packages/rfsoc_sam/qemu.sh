51f120ddefd59fcdf445f1a989f06abb3fc77410
Lewis Brown <lewisb@xilinx.com>
2021-10-14 15:44:22 +0100

    fixed pip install

--- board/RFSoC2x2/packages/rfsoc_sam/qemu.sh
+++ board/RFSoC2x2/packages/rfsoc_sam/qemu.sh
@@ -18,7 +18,7 @@ if [ ! -d "/root/rfsoc_sam_build" ]; then
 	git clone https://github.com/schelleg/rfsoc_sam rfsoc_sam_build
 fi
 cd /root/rfsoc_sam_build
-python3 -m pip install rfsoc_sam
+python3 -m pip install .
 
 cd /root
 rm -rf rfsoc_sam_build
